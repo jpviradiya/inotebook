9207f91441f61a5c52aeed1b8fc187b092f00fd9
Jeel Viradiya <jeelviradiya188@gmail.com>
2023-06-24 17:03:11 +0530

    commit

---
+++ backend/db.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose')
+const mongoURI = "mongodb+srv://jeelviradiya188:YPLkMEGMsBWzJYfo@mongo-atlas.r3opsa6.mongodb.net/"
+
+const connectToMongo = async () => {
+  await mongoose.connect(mongoURI)
+  // console.log(mongoose.connection.readyState)
+}
+module.exports = connectToMongo
