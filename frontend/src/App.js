ad70f7090598d39833a48e81649f96aa7205c295
Jeel Viradiya <jeelviradiya188@gmail.com>
2023-06-26 18:38:29 +0530

    commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,25 @@
 import './App.css';
-
+import About from './components/About';
+import Home from './components/Home';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NoteState from './context/notes/NoteState';
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
-  );
+	return (
+		<>
+			<NoteState>
+				<Router>
+					<Navbar />
+					<div className="container">
+						<Routes>
+							<Route exact path="/" element={<Home />}></Route>
+							<Route exact path="/about" element={<About />}></Route>
+						</Routes>
+					</div>
+				</Router>
+			</NoteState>
+		</>
+	);
 }
 
 export default App;
---
+++ frontend/src/context/notes/NoteState.js
@@ -0,0 +1,33 @@
+import NoteContext from "./noteContext";
+import { useState } from "react";
+
+const NoteState = (props) => {
+	const notesInitial = [
+		{
+			"_id": "649837cd7c7bb5e9cbc5854c",
+			"user": "6498351dd11e044f3c1cf92f",
+			"title": "jeel viradiya",
+			"description": "Hello jeel viradiya how are you",
+			"tag": "personal",
+			"date": "2023-06-25T12:49:17.938Z",
+			"__v": 0
+		},
+		{
+			"_id": "649837cd7c7bb5e9cbc5854d",
+			"user": "6498351dd11e044f3c1cf92f",
+			"title": "ami viradiya",
+			"description": "Hello ami viradiya how are you",
+			"tag": "personal",
+			"date": "2023-06-25T12:49:17.938Z",
+			"__v": 0
+		}
+	]
+	const [notes, setNotes] = useState(notesInitial)
+	return (
+		<NoteContext.Provider value={{notes,setNotes}}>
+			{props.children}
+		</NoteContext.Provider>
+	)
+}
+
+export default NoteState;
